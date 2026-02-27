20b17cf8b78985bdcca3b76b9f02311c14f25483
zaap38 <zappeurben@gmail.com>
2026-02-27 18:21:57 +0100

    adding register var name

---
+++ registers.py
@@ -0,0 +1,18 @@
+rax = "rax"
+rbx = "rbx"
+rcx = "rcx"
+rdx = "rdx"
+
+rsi = "rsi"
+rdi = "rdi"
+rbp = "rbp"
+rsp = "rsp"
+
+r8  = "r8"
+r9  = "r9"
+r10 = "r10"
+r11 = "r11"
+r12 = "r12"
+r13 = "r13"
+r14 = "r14"
+r15 = "r15"
